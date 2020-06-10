fe14048c6e23b855bae89597bcef2351af26a73a
Alina Nerlich <alina.nerlich@gmx.de>
2020-06-10 15:40:04 +0200

    Verbesserung fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,11 +3,11 @@ print("Welcome to FizzBuzz")
 number = int(input("Please select a number between 1 and 100: "))
 
 for x in range(1, number+1):
-    if x % 3 == 0:
-        print("fizz")
+    if x % 3 == 0 and x % 5 == 0:
+        print("fizzbuzz")
     elif x % 5 == 0:
         print("buzz")
-    elif x % 3 == 0 and x % 5 == 0:
-        print("fizzbuzz")
+    elif x % 3 == 0:
+        print("fizz")
     else:
         print(x)
